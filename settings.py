c99290802fc21753099c1f0df42ae13dd237f090
Jack206Man <95452460+Jack206Man@users.noreply.github.com>
2025-07-27 03:51:37 -0400

    Video 3 (Unit 11)
    Added content from the Third Video in Unit 11 (Created ship class and set up drawing it to the screen).

--- settings.py
+++ settings.py
@@ -8,3 +8,7 @@ class Settings:
         self.screen_h = 400
         self.FPS = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'ImagesProvided' / 'Starbasesnow.png'
+
+        self.ship_file = Path.cwd() / 'Assets' / 'images' / "ImagesProvided" / 'ship2(no bg).png'
+        self.ship_w = 40
+        self.ship_h = 60
