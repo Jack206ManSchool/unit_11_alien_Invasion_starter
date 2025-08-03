db8c1d0f32726485781e44308f6d5ed7953f8885
Jack206Man <95452460+Jack206Man@users.noreply.github.com>
2025-08-02 23:17:44 -0400

    Added customizable fleet list
    Added a customizable fleet list

--- settings.py
+++ settings.py
@@ -4,12 +4,12 @@ class Settings:
 
     def __init__(self):
         self.name: str = 'Alien Invasion'
-        self.screen_w = 600
-        self.screen_h = 400
+        self.screen_w = 800
+        self.screen_h = 600
         self.FPS = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'ImagesProvided' / 'Starbasesnow.png'
 
-        self.ship_file = Path.cwd() / 'Assets' / 'images' / "ImagesProvided" / 'ship2(no bg).png'
+        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship.png'
         self.ship_w = 40
         self.ship_h = 60
         self.ship_speed = 5
